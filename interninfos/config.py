1b8a9a004da1d7065f2ef89f6e29a60559eadd30
PotlapelliHarshita <potlapelliharshita@gmail.com>
2025-08-20 21:09:37 +0530

    Update config.py

--- interninfos/config.py
+++ interninfos/config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '96082683'
-    MYSQL_DB = 'UserDetails'
+    MYSQL_HOST = 'YOUR HOSTNAME'
+    MYSQL_USER = 'YOUR USER'
+    MYSQL_PASSWORD = 'YOUR PASSWORD'
+    MYSQL_DB = 'DATABASE NAME'
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
