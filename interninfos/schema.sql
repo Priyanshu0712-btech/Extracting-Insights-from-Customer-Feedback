fabbeefd2ee7c5ea9e3fc9a3b1fe5ec57d69dc64
Priyanshu Kumar <149661471+Priyanshu0712-btech@users.noreply.github.com>
2025-08-21 15:57:49 +0530

    Update schema.sql

--- interninfos/schema.sql
+++ interninfos/schema.sql
@@ -8,3 +8,17 @@ CREATE TABLE users (
     password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+CREATE TABLE IF NOT EXISTS reviews (
+    review_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    review_text TEXT NOT NULL,
+    product_id VARCHAR(100) NULL,
+    category VARCHAR(100),
+    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    overall_sentiment VARCHAR(50),
+    overall_sentiment_score FLOAT,
+    
+    CONSTRAINT fk_reviews_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
