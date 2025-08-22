7d7f1a2093ff9e4ee2af7256b7a0b68f847a6856
jk-117 <jaisimhak117@gmail.com>
2025-08-22 16:15:17 +0530

    Safe and secure sharing of config.py file hiding sensitive details

--- interninfos/config.py
+++ interninfos/config.py
@@ -5,9 +5,9 @@ load_dotenv()
 
 class Config:
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_PORT = int(os.getenv('MYSQL_PORT', '3306'))
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'JaiK_117')
+    MYSQL_PORT = int(os.getenv('MYSQL_PORT', 'Your_Port'))
+    MYSQL_USER = os.getenv('MYSQL_USER', 'Your_Username')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'Your_Password')
     MYSQL_DB = os.getenv('MYSQL_DB', 'customer_feedback_db')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
