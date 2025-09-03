36daff8d7e7201ad824738b8ea0720ac1f97adff
Jaisimha Kothari <jaisimhak117@gmail.com>
2025-09-03 17:18:55 +0530

    admin sql schema updated

--- interninfos/schema.sql
+++ interninfos/schema.sql
@@ -23,3 +23,9 @@ CREATE TABLE IF NOT EXISTS reviews (
     
     CONSTRAINT fk_reviews_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
+
+CREATE TABLE admins (
+    admin_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL
+);
